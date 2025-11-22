a607bb4e4b6a4e4a65d273cf6a445823294f56cb
Reinaldo Cancino <reinaldoallen@macro.com.ar>
2025-11-21 21:03:53 -0300

    delete dev

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import HomeClient from "./[lang]/home-client";
-import { getDictionary } from "@/dictionaries/dictionaries";
-import { i18n } from "@/dictionaries/i18n-config";
-
-export default async function Home() {
-  const lang = i18n.defaultLocale;
-  const dictionary = await getDictionary(lang);
-  return <HomeClient lang={lang} t={dictionary.home} />;
-}
